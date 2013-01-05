ccbd4db2b0983680a2bb3f1b493189c53deb4dea
sandeepshetty <sandeep.shetty@gmail.com>
2013-01-06 02:07:40 +0530

    Bugfix and formatting of examples.

--- example.php
+++ example.php
@@ -28,18 +28,38 @@
 
 	# Example 5: Passing a custom request header
 	# The application/json content-type will automatically convert the POST payload array into a json string.
-	$response_body = http\request('POST http://httpbin.org/post', array(), array('hello'=>'world', 'foo'=>'bar'), $response_headers, array('content-type'=>'application/json; charset=utf-8'));
+	$response_body = http\request
+	(
+		'POST http://httpbin.org/post',
+		array(),
+		array('hello'=>'world', 'foo'=>'bar'),
+		$response_headers,
+		array('content-type'=>'application/json; charset=utf-8')
+	);
 	print_r($response_body);
 
 
 	# Example 6: Passing an overriden cURL opt
-	$response_body = http\request('POST http://httpbin.org/post', array(), array('hello'=>'world', 'foo'=>'bar'), $response_headers, array('content-type'=>'application/json; charset=utf-8'), array(CURLOPT_USERAGENT=>'MY_APP_NAME'));
+	$response_body = http\request
+	(
+		'POST http://httpbin.org/post',
+		array(),
+		array('hello'=>'world', 'foo'=>'bar'),
+		$response_headers,
+		array('content-type'=>'application/json; charset=utf-8'),
+		array(CURLOPT_USERAGENT=>'MY_APP_NAME')
+	);
 	print_r($response_body);
 
 
 	# Example 7: Creating an instance
 	# If you're making multiple HTTP calls with the same base URI / request headers / $curl_opts, do this instead:
-	$http_client = http\client('http://httpbin.org', array('content-type'=>'application/json; charset=utf-8'), array(CURLOPT_USERAGENT=>'MY_APP_NAME'))
+	$http_client = http\client
+	(
+		'http://httpbin.org',
+		array('content-type'=>'application/json; charset=utf-8'),
+		array(CURLOPT_USERAGENT=>'MY_APP_NAME')
+	);
 	$response_body = $http_client('POST /post', array(), array('hello'=>'world', 'foo'=>'bar'));
 	print_r($response_body);
 
