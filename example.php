1b8345b343422f8387a173656d71d4bf307ab85f
sandeepshetty <sandeep.shetty@gmail.com>
2013-01-07 03:15:32 +0530

    Formatting examples.

--- example.php
+++ example.php
@@ -56,12 +56,7 @@
 
 		# Example 7: Creating an instance
 		# If you're making multiple HTTP calls with the same base URI / request headers / $curl_opts, do this instead:
-		$http_client = http\client
-		(
-			'http://httpbin.org',
-			array('content-type'=>'application/json; charset=utf-8'),
-			array(CURLOPT_USERAGENT=>'MY_APP_NAME')
-		);
+		$http_client = http\client('http://httpbin.org', array('content-type'=>'application/json; charset=utf-8'), array(CURLOPT_USERAGENT=>'MY_APP_NAME'));
 		$response_body = $http_client('POST /post', array(), array('hello'=>'world', 'foo'=>'bar'));
 		print_r($response_body);
 
